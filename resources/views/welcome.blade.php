f27109507742fed3c2c928a5b6d6c5e13d7be0d1
fauzanashshidiq <muhzeen07@gmail.com>
2025-12-23 12:21:48 +0700

    change to https & add icon, manifest, sw for vercel support

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
     
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
     <meta name="theme-color" content="#4F46E5">
     <link rel="apple-touch-icon" href="{{ asset('icons/icon-192.png') }}">
